fa9fbf3c56675bb4e4898c108b8ebc74b9a99ba6
Darkest_Time <timofey.suhanov2005@gmail.com>
2026-04-29 14:14:32 +0500

    Add tests

--- TimeAndWeather/tests.py
+++ TimeAndWeather/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from unittest.mock import patch
+from .models import City
 
-# Create your tests here.
+class WeatherViewTests(TestCase):
+    def setUp(self):
+        city_names = ["Москва", "Moscow"]
+        for name in city_names:
+            City.objects.create(name=name)
+
+    @patch('requests.get')
+    def test_status_code(self, mock_get):
+        mock_get.return_value.status_code = 500
+
+        response = self.client.get('/')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.context['weather_data']), 0)
+
+    @patch('requests.get')
+    def test_api_exception(self, mock_get):
+        mock_get.side_effect = Exception("API is down")
+
+        response = self.client.get('/')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.context['weather_data']), 0)
